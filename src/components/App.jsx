5e29beca3e2efe17d0f63ba35397d2c6604ab84d
AndreyMachula <amcz2020@gmail.com>
2022-07-17 22:20:54 +0200

    adds react-toastify and react-icons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import { ToastContainer } from 'react-toastify';
+import Searchbar from './Searchbar/Searchbar';
+
+export default class App extends Component {
+  handleSearchbarSubmit = search => {
+    console.log(search)
+  }
+ 
+  render() {
+    return (
+      <div className='App'>
+        <Searchbar onSubmit={this.handleSearchbarSubmit}/>
+      </div>
+    );
+  }
+}
